cb1e696e28c4ec03d9bf36d5929408aedf6566aa
Gabriel D. Celery <gabriel.d.celery@gmail.com>
2016-01-20 16:38:54 +0100

    Fixed the queries in the shortlist search function

--- src/php/companies/form_search_companies.php
+++ src/php/companies/form_search_companies.php
@@ -33,32 +33,40 @@ $pdo = $connection->connect();
 QUERY
 ************************************************************/
 
-$subquery_by_contract_status = '(';
-$subquery_by_contract_status .= 'SELECT
-	companies_detailed.id AS company_id,
-	company_name,
-	contract_status,
-	company_email,
-	company_phone,
-	starting_date,
-	ending_date,
-	postal_number
-	FROM companies
-	INNER JOIN companies_detailed ON companies.id = companies_detailed.company_id';
+/* Filter by last contract */
+
+$subquery_by_last_contract = '(';
+$subquery_by_last_contract .= 'SELECT companies.id AS id, company_name, contract_status, ';
+
+if($last_contract_only == true){
+	$subquery_by_last_contract .= 'MAX(ending_date) AS ending_date ';
+} else {
+	$subquery_by_last_contract .= 'ending_date ';
+}
+
+$subquery_by_last_contract .= 'FROM companies INNER JOIN companies_detailed ON companies.id = companies_detailed.company_id ';
+
+if($last_contract_only == true){
+	$subquery_by_last_contract .= 'GROUP BY company_name';
+}
+
+$subquery_by_last_contract .= ') ';
+
+/* Filter by company name */
+
+if($company_name != "" || $company_name != null){
+	$subquery_by_company_name = '(';
+	$subquery_by_company_name .= 'SELECT * FROM ' . $subquery_by_last_contract . ' AS filtered_by_last_contract ';
+	$subquery_by_company_name .= ' WHERE company_name = "' . $company_name . '"';
+	$subquery_by_company_name .= ')';
+} else {
+	$subquery_by_company_name = $subquery_by_last_contract;
+}
+
+/* Filter by contracts status */
 
 $subquery_by_contract_status = '(';
-$subquery_by_contract_status .= 'SELECT
-	companies_detailed.id AS company_id,
-	company_name,
-	manager_name,
-	contract_status,
-	company_email,
-	company_phone,
-	starting_date,
-	ending_date,
-	postal_number
-	FROM companies
-	INNER JOIN companies_detailed ON companies.id = companies_detailed.company_id';
+$subquery_by_contract_status .= 'SELECT * FROM ' . $subquery_by_company_name . ' AS filtered_company_name ';
 
 if ($valid_contract == true && $expired_contract == true){
 	$subquery_by_contract_status .= ' WHERE contract_status = true OR contract_status = false';
@@ -69,46 +77,43 @@ if ($valid_contract == true && $expired_contract == true){
 } else {
 	$subquery_by_contract_status .= ' AND contract_status = true AND contract_status = false';	
 }
-$subquery_by_contract_status .= ' ORDER BY ending_date DESC ';
+
 $subquery_by_contract_status .= ') ';
 
-/* Filtering by last contract */
+/* Joining tables */
 
-if($last_contract_only == true){
-	$subquery_by_last_contract = '(';
-	$subquery_by_last_contract .= 'SELECT * FROM ' . $subquery_by_contract_status . ' AS filtered_by_contract_status ';
-	$subquery_by_last_contract .= ' GROUP BY company_name';
-	$subquery_by_last_contract .= ') ';
-} else {
-	$subquery_by_last_contract = $subquery_by_contract_status;
-}
+$subquery_jointables = '(';
+$subquery_jointables .= 'SELECT 
+	companies_detailed.id AS id, 
+	company_name, 
+	manager_name, 
+	contract_status, 
+	company_email, 
+	company_phone, 
+	starting_date, 
+	companies_detailed.ending_date AS ending_date, 
+	postal_number 
+	FROM ' . $subquery_by_contract_status . ' AS filtered_contract_status ' .
+	'INNER JOIN companies_detailed ON 
+	filtered_contract_status.id = companies_detailed.company_id AND
+	filtered_contract_status.ending_date = companies_detailed.ending_date';
+$subquery_jointables .= ') ';	
 
-/* Filtering by company name */
-
-if($company_name != "" || $company_name != null){
-	$subquery_by_company_name = '(';
-	$subquery_by_company_name .= 'SELECT * FROM ' . $subquery_by_last_contract . ' AS filtered_by_last_contract';
-	$subquery_by_company_name .= ' WHERE company_name = "' . $company_name . '"';
-	$subquery_by_company_name .= ')';
-} else {
-	$subquery_by_company_name = $subquery_by_last_contract;
-}
-
-/* Filtering by manager name */
+/* Filter by manager name */
 
 if($manager_name != "" || $manager_name != null){
 	$subquery_by_manager_name = '(';
-	$subquery_by_manager_name .= 'SELECT * FROM ' . $subquery_by_company_name . ' AS filtered_by_company_name';
+	$subquery_by_manager_name .= 'SELECT * FROM ' . $subquery_jointables . ' AS joined_tables';
 	$subquery_by_manager_name .= ' WHERE manager_name = "' . $manager_name . '"';
 	$subquery_by_manager_name .= ')';
 } else {
-	$subquery_by_manager_name = $subquery_by_company_name;
+	$subquery_by_manager_name = $subquery_jointables;
 }
 
-/* Filtering by date interval and order them by date */
+/* Filtering by date interval */
 
 $subquery_by_date_interval = '(';
-$subquery_by_date_interval .= 'SELECT * FROM ' . $subquery_by_manager_name . ' AS filtered_by_manager_name ';
+$subquery_by_date_interval .= 'SELECT * FROM ' . $subquery_by_manager_name . ' AS filtered_by_date_interval ';
 $subquery_by_date_interval .= ' WHERE 1';
 if ($starting_date != null){
 	$subquery_by_date_interval .= ' AND ending_date >= "' . $starting_date . '" ';
@@ -116,6 +121,7 @@ if ($starting_date != null){
 if ($ending_date != null){
 	$subquery_by_date_interval .= ' AND ending_date <= "' . $ending_date . '" ';
 }
+
 $subquery_by_date_interval .= ') ';
 
 $mainquery = $subquery_by_date_interval;
